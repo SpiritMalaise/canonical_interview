637ed737f74b03eb4f65e60c212dfe0d6a489bb2
Oliver Tassell <olivertassell@hotmail.com>
2026-03-12 19:51:37 +0000

    made use of filestream instead of writing files

--- src/FileProcessor.py
+++ src/FileProcessor.py
@@ -4,33 +4,31 @@ import csv
 
 def processFile(file: FileStorage):
 	fieldnames = ['Date', 'Type', 'Amount($)', 'Memo']
-	filepath = './uploads/'
-
-	# Save the file to a processing location
-	file.save(filepath + 'processing_data.csv')
 
 	revenue = 0
 	expenses = 0
-	with open(filepath + 'processing_data.csv') as csvfile:
-		reader = csv.reader(csvfile, delimiter=',')
-		for row in reader:
-			# Ignore rows that don't conform to expected length or types
-			if len(row) == 4:
-				for index, item in enumerate(row):
-					row[index] = item.strip().lower()
-				if row[1] == 'income':
-					revenue += float(row[2])
-				elif row[1] == 'expense':
-					expenses += float(row[2])
-			else:
-				continue
+
+	file.seek(0)
+	bytestring = file.read()
+	for row in bytestring.decode('utf-8').splitlines():
+		# Ignore rows that don't conform to expected length or types
+		row = row.split(', ')
+		if len(row) == 4:
+			for index, item in enumerate(row):
+				row[index] = item.strip().lower()
+			if row[1] == 'income':
+				revenue += float(row[2])
+			elif row[1] == 'expense':
+				expenses += float(row[2])
+		else:
+			continue
 
 	result = {
 		"gross-revenue": round(revenue,2),
-		"expenses": round(expenses),
-		"net-revenue": round(revenue - expenses)
+		"expenses": round(expenses,2),
+		"net-revenue": round(revenue - expenses,2)
 	}
 	
-	with open(filepath + 'result.json', 'x') as file:
+	with open('./data/result.json', 'w+') as file:
 		dump(result, file)
 	return 'OK', 200
