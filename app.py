40a612dc21b99b2c86f7420a91894b54537e94ea
Oliver Tassell <olivertassell@hotmail.com>
2026-03-12 17:06:57 +0000

    added health check route

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-	return "<p>Hello, World!</p>"
+@app.route("/health")
+def health_check():
+	return  'OK', 200
