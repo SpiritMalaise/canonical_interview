c3e4761b3267a36614adf4128b6df201450675d9
Oliver Tassell <olivertassell@hotmail.com>
2026-03-12 17:08:39 +0000

    added framework required request routes

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/health")
 def health_check():
 	return  'OK', 200
+
+@app.post("/transactions")
+def transactions():
+	return 501
+
+@app.post("/report")
+def transactions():
+	return 501
